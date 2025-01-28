63959eeb8a8566c64d8b261e4492a4ce854e5438
hanfu2022 <83848191+hanfu2022@users.noreply.github.com>
2025-01-28 16:34:02 +0800

    Update player.js

--- player.js
+++ player.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function() {
     let channelList = [];
 
     // Load the channel list from the channels.txt file
-    const channelsTxtUrl = 'channels.txt';
+    const channelsTxtUrl = 'https://etxt.chickenkiller.com/channels.txt';
 
 fetch(channelsTxtUrl)  // 这里将路径改成远程路径
         .then(response => response.text())
